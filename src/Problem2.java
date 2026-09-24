6cc29d678fa0acacf767dae3d095e69df8bf5cbe
Tamim Farhan <tamimf2@nycstudents.net>
2026-09-24 15:36:08 -0400

    U1T3

--- src/Problem2.java
+++ src/Problem2.java
@@ -1,2 +1,24 @@
-public class Problem2 {
+import java.util.Scanner;
+public class Problem2
+{
+    void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Player 1 enter your name:");
+        String p1Name = scanner.nextLine();
+        System.out.println("Player 1 enter your decimal number:");
+        double p1Num = scanner.nextDouble();
+        System.out.println("Player 2 enter your name:");
+        String p2Name = scanner.nextLine();
+        System.out.println("Player 2 enter your decimal number:");
+        double p2Num = scanner.nextDouble();
+        if (p1Num > p2Num) {
+            System.out.println(p1Name + "'s number is larger.");
+            if (p1Num < p2Num) {
+                System.out.println(p2Name + "'s number is larger.");
+            }
+            if (p1Num == p2Num) {
+                System.out.println("The numbers are equal!");
+            }
+        }
+    }
 }
